1f1cd28b22a8f6e4ad232be27b9602d45037bf1d
Matias Martinez <mattmartinez05a@gmail.com>
2023-08-02 00:47:18 -0300

    login

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const PORT = 3000;
 
 app.use(express.static('public'));
 
-app.get('/',(erq, res) => 
- res.sendFile(path.join(__dirname,'views','home.html')))
+app.get('/',(erq, res) => res.sendFile(path.join(__dirname,'views','home.html')))
+app.get('/login',(erq, res) => res.sendFile(path.join(__dirname,'views','home.html')))
+app.get('/registro',(erq, res) => res.sendFile(path.join(__dirname,'views','home.html')))
 
 app.listen(PORT, () => console.log(`server running in http://localhost:${PORT}`))
